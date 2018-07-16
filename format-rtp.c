47f435603638412b76d3687e902e2d6f9cfc5423
Jan Starý <hans@stare.cz>
2018-07-16 16:12:47 +0200

    RTP timestamp is 32 bits

--- format-rtp.c
+++ format-rtp.c
@@ -9,7 +9,7 @@ print_rtphdr(struct rtphdr* rtp)
 	if (rtp == NULL)
 		return;
 	fprintf(stderr, " %c version %u, ts %u, seq %u, ssrc %#x, pt %u\n",
-		rtp->m ? '*' : ' ', rtp->v, ntohs(rtp->ts),
+		rtp->m ? '*' : ' ', rtp->v, ntohl(rtp->ts),
 		ntohs(rtp->seq), ntohs(rtp->ssrc), rtp->pt);
 	if (rtp->cc) {
 		fprintf(stderr, "   (sources: ");
